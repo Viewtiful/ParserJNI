8e8a83d5815557fe7b9dc8375dd82e7be651380f
baptiste <baptiste.haudegand@gmail.com>
2013-06-18 13:53:56 +0200

    cleaned OutputCpp

--- JNA/JNI/OutputCpp.h
+++ JNA/JNI/OutputCpp.h
@@ -10,31 +10,30 @@ using namespace nsC;
 
 namespace nsJNI
 {
-
 	class OutputCpp{
-	
-	public:
-		OutputCpp(TypesDictionnary *dictionnary);
-		~OutputCpp();
-		void addCppType(std::string inputType, std::string cppType);
-		void addVMSignature(string inputType, string vmSignature);
-		string getCppType(string inputType);
-		string getVMSignature(string inputType);
-		void printPrototype(string typeRetour);
-		void printName(string name);
-		void printParameters(Param::vector& parameters);
-		void printParameter(Param parameter);
-		void convert(Module& module);
-		void addInclude();
-		void addContent();
-      void generateJNIOnload(string filename);
-		
-	protected:	
-		map<string,string> toCppType;
-		map<string, string> toVMSignature;
-		ofstream outputFile;		
-		TypesDictionnary *dictionnary;
+
+		public:
+			OutputCpp(TypesDictionnary *dictionnary);
+			~OutputCpp();
+			void addCppType(std::string inputType, std::string cppType);
+			void addVMSignature(string inputType, string vmSignature);
+			string getCppType(string inputType);
+			string getVMSignature(string inputType);
+			void printPrototype(string typeRetour);
+			void printName(string name);
+			void printParameters(Param::vector& parameters);
+			void printParameter(Param parameter);
+			void convert(Module& module);
+			void addInclude();
+			void addContent();
+			void generateJNIOnload(string filename);
+			void addFunctionPrototype(Function::vector fcts);
+			void addNativeFunctionTable(Function::vector fcts, string fileName);
+
+		protected:	
+			map<string,string> toCppType;
+			map<string, string> toVMSignature;
+			ofstream outputFile;		
+			TypesDictionnary *dictionnary;
 	};
-
 }
-
