79963da6054e12b62bac1f8c45a0fd2167b4d094
Baptiste Haudegand <baptiste.haudegand@gmail.com>
2013-07-18 17:38:27 +0200

    Added comments to AddressWrapper

--- JNA/JNI/Types/AddressWrapper.cpp
+++ JNA/JNI/Types/AddressWrapper.cpp
@@ -16,24 +16,20 @@ AddressWrapper::AddressWrapper(string realCType,string VMSignature) : Type("Addr
 	nbOfRtn = 1;
 }
 
-
-
 AddressWrapper::~AddressWrapper()
 {
-
 }
-
 
 std::string AddressWrapper::outputJava()
 {
 	return getJavaType();
 }
- 
+
 std::string AddressWrapper::outputJNI()
 {
 	return getJNIType();
 }
-        
+
 bool AddressWrapper::isNativeType()
 {
 	return false;
@@ -56,28 +52,29 @@ bool AddressWrapper::isArray()
 
 void AddressWrapper::prepareCall(ofstream& f,string& varName)
 {
-   //We create the C object equivalent of the Java one and allocate memory for it.
-   string structure (
-         "\t\t%TYPE% %NAME%;\n"
-         "\t\tcontextWrapper *ctxWrp%VALUE% = (contextWrapper *)malloc(sizeof(contextWrapper));\n"
-         "\t\tctxWrp%VALUE%->env = env;\n\n"
-         );
-   std::ostringstream oss;
-   oss << nbOfOcc;
-   _varName = varName;
+	//We create the C object equivalent of the Java one and allocate memory for it.
+	string structure (
+			"\t\t%TYPE% %NAME%;\n"
+			"\t\tcontextWrapper *ctxWrp%VALUE% = (contextWrapper *)malloc(sizeof(contextWrapper));\n"
+			"\t\tctxWrp%VALUE%->env = env;\n\n"
+			);
+	std::ostringstream oss;
+	oss << nbOfOcc;
+	_varName = varName;
 
-   string name = "C_" + varName;
-   if(_realCType.find("*", 0) != string::npos) {   //A optimiser
-      stringReplace(structure, "TYPE", _realCType.substr(0, _realCType.size() -2));
-   }
-   else
-      stringReplace(structure, "TYPE", _realCType);
-   
-   stringReplace(structure, "NAME", name);
-   stringReplace(structure, "VALUE", oss.str());
-   f << structure;
-   //We don't forget to increment the value, in order to have each object unique.
-   nbOfOcc = nbOfOcc + 1;
+	string name = "C_" + varName;
+	//If it's a pointer, we suppress it from the type.
+	if(_realCType.find("*", 0) != string::npos) {   //A optimiser
+		stringReplace(structure, "TYPE", _realCType.substr(0, _realCType.size() -2));
+	}
+	else
+		stringReplace(structure, "TYPE", _realCType);
+
+	stringReplace(structure, "NAME", name);
+	stringReplace(structure, "VALUE", oss.str());
+	f << structure;
+	//We don't forget to increment the value, in order to have each object unique.
+	nbOfOcc = nbOfOcc + 1;
 }
 
 string AddressWrapper::getJNIParameterName(string& varName)
@@ -87,34 +84,34 @@ string AddressWrapper::getJNIParameterName(string& varName)
 }
 void AddressWrapper::getReturnValue(ofstream& f)
 {
-   //We return the address of the C object to Java.
-   string structure (
-         "\t\tctxWrp%VALUE%->ctxRef = %NAME%;\n"
-         "\t\tjclass adrWrp_%CNAME%;\n"
-         "\t\tjmethodID setAddr_%CNAME%;\n"
-         "\t\tjlong arg_%CNAME%;\n\n"
-         "\t\tadrWrp_%CNAME% = (*env)->GetObjectClass(env, %CNAME%);\n"
-         "\t\tsetAddr_%CNAME% = (*env)->GetMethodID(env, adrWrp_%CNAME%, \"setAddress\", \"(J)V\");\n"
-         "\t\targ_%CNAME% = (jlong) ctxWrp%VALUE%;\n"
-         "\t\t(*env)->CallVoidMethod(env, %CNAME%, setAddr_%CNAME%, arg_%CNAME%);\n\n"
-         );
+	//We return the address of the C object to Java.
+	string structure (
+			"\t\tctxWrp%VALUE%->ctxRef = %NAME%;\n"
+			"\t\tjclass adrWrp_%CNAME%;\n"
+			"\t\tjmethodID setAddr_%CNAME%;\n"
+			"\t\tjlong arg_%CNAME%;\n\n"
+			"\t\tadrWrp_%CNAME% = (*env)->GetObjectClass(env, %CNAME%);\n"
+			"\t\tsetAddr_%CNAME% = (*env)->GetMethodID(env, adrWrp_%CNAME%, \"setAddress\", \"(J)V\");\n"
+			"\t\targ_%CNAME% = (jlong) ctxWrp%VALUE%;\n"
+			"\t\t(*env)->CallVoidMethod(env, %CNAME%, setAddr_%CNAME%, arg_%CNAME%);\n\n"
+			);
 
-   std::ostringstream oss;
-   oss << nbOfRtn;
-   string name = "C_" + _varName;
-   if(_realCType.find("*", 0) != string::npos) {
-      stringReplace(structure, "TYPE", _realCType.substr(0, _realCType.size() -2));
-   }
-   else
-      stringReplace(structure, "TYPE", _realCType);
-   
-   stringReplace(structure, "CNAME", _varName);
-   stringReplace(structure, "VALUE", oss.str());
-   stringReplace(structure, "NAME", name);
-   
-   f << structure;
-   //We don't forget to increment the value, in order to have each the object corresponding to prepareCall()
-   //and each of them to be unique.
-   nbOfRtn = nbOfRtn + 1;
+	std::ostringstream oss;
+	oss << nbOfRtn;
+	string name = "C_" + _varName;
+	if(_realCType.find("*", 0) != string::npos) {
+		stringReplace(structure, "TYPE", _realCType.substr(0, _realCType.size() -2));
+	}
+	else
+		stringReplace(structure, "TYPE", _realCType);
+
+	stringReplace(structure, "CNAME", _varName);
+	stringReplace(structure, "VALUE", oss.str());
+	stringReplace(structure, "NAME", name);
+
+	f << structure;
+	//We don't forget to increment the value, in order to have each the object corresponding to prepareCall()
+	//and each of them to be unique.
+	nbOfRtn = nbOfRtn + 1;
 }
-        
+
