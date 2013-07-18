6c2f54e7156bf21943159b414c547a0aaca5892d
Baptiste Haudegand <baptiste.haudegand@gmail.com>
2013-07-18 10:51:33 +0200

    Re-work of test for a hash of a string

--- JNA/Java/tests/hash/hash.java
+++ JNA/Java/tests/hash/hash.java
@@ -1,27 +1,48 @@
-public class hash
-{
-   public static void main(String[] args) {
-      ArcanaJNI aJNI = new ArcanaJNI();
+public class hash {
+	public static void main(String[] args) {
+		ArcanaJNI aJNI = new ArcanaJNI();
 
-      aJNI.ktb_init();
+		if (aJNI.ktb_init() == ArcanaJNI.ktb_errno.KTB_ERR_NO_ERROR)
+			System.out.println(Utils.getCurrentTime() + "ktb_init() ok");
+		else {
+			System.out.println(Utils.getCurrentTime()
+					+ "Error while initializating ktb_init()");
+			System.exit(1);
+		}
 
-      String hello = "Hello1";
-      byte[] helloB = hello.getBytes();
+		if (!hash.hashString(aJNI)) {
+			aJNI.ktb_clear();
+			System.exit(1);
+		}
 
-      ArcanaJNI.ktb_hash_algo_t HASH_ALGO = ArcanaJNI.ktb_hash_algo_t.KTB_HASH_ALGO_SHA512;
+		aJNI.ktb_clear();
+	}
 
-      long hash_len = aJNI.ktb_hash_get_len_from_algo(HASH_ALGO);
-      System.out.println("hash_len : " + hash_len);
+	public static boolean hashString(ArcanaJNI aJNI) {
+		String hello = "Hello1";
+		byte[] helloB = hello.getBytes();
 
-      byte[] hash = new byte[(int) (long)hash_len];
+		ArcanaJNI.ktb_hash_algo_t HASH_ALGO = ArcanaJNI.ktb_hash_algo_t.KTB_HASH_ALGO_SHA512;
 
-      aJNI.ktb_hash_block(HASH_ALGO, helloB, hash);
+		long hash_len = aJNI.ktb_hash_get_len_from_algo(HASH_ALGO);
+		System.out.println("hash_len : " + hash_len);
 
-      StringBuilder sb = new StringBuilder();
-      for(byte b : hash) 
-         sb.append(String.format("%02X ", b));
-      System.out.println(sb.toString());
+		byte[] hash = new byte[(int) (long) hash_len];
 
-      aJNI.ktb_clear();
-   }
+		if (aJNI.ktb_hash_block(HASH_ALGO, helloB, hash) == ArcanaJNI.ktb_errno.KTB_ERR_NO_ERROR)
+			System.out.println(Utils.getCurrentTime() + "ktb_hash_block() ok");
+		else {
+			System.out.println(Utils.getCurrentTime()
+					+ "Error while initializating ktb_hash_block()");
+			Utils.freeBuffer(helloB);
+			Utils.freeBuffer(hash);
+			return false;
+		}
+
+		Utils.printBuffer(hash);
+
+		Utils.freeBuffer(helloB);
+		Utils.freeBuffer(hash);
+		return true;
+	}
 }
