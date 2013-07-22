189ba3afa2a343d448e132e241aaf385dd66370a
Baptiste Haudegand <baptiste.haudegand@gmail.com>
2013-07-22 17:08:44 +0200

    Identation and comments

--- JNA/JNI/Types/Struct.cpp
+++ JNA/JNI/Types/Struct.cpp
@@ -20,7 +20,6 @@ Struct::Struct(ofstream &f, ofstream &f2, const string& VMSignature, const nsC::
 	this->_javaType = cStruct.getTypedef();
 }
 
-
 std::string Struct::outputJava() // Warning unused overridden function
 {
 	return "Java";
@@ -32,10 +31,10 @@ std::string Struct::outputJNI() // Warning unused overridden function
 }
 
 void Struct::addStructFunctionToJNI(ofstream &f) {
-   _createFunction->convertJNI(f);
-   _freeFunction->convertJNI(f);
-   _structSize->convertJNI(f);
-   
+	_createFunction->convertJNI(f);
+	_freeFunction->convertJNI(f);
+	_structSize->convertJNI(f);
+
 	for(size_t i = 0;i<_getters.size();i++)
 		_getters[i]->convertJNI(f);
 
@@ -45,89 +44,88 @@ void Struct::addStructFunctionToJNI(ofstream &f) {
 
 void Struct::addStructToJava(ofstream &f)
 {
-   string structure(
-		"\tpublic class %CLASSNAME% {\n"
-		"%FIELDS%\n"
-      "\t\tlong struct_size;\n"
-      "\t\tprivate AddressWrapper mInternal;\n\n"
-      "%CONSTRUCTOR%"
-      "%FINALIZE%"
-      "%MINTERNAL%"
-      "%WRITE%"
-      "%READ%"
-		"\t}\n\n"
-	);
+	string structure(
+			"\tpublic class %CLASSNAME% {\n"
+			"%FIELDS%\n"
+			"\t\tlong struct_size;\n"
+			"\t\tprivate AddressWrapper mInternal;\n\n"
+			"%CONSTRUCTOR%"
+			"%FINALIZE%"
+			"%MINTERNAL%"
+			"%WRITE%"
+			"%READ%"
+			"\t}\n\n"
+			);
 
-   _createFunction = new Create(_javaType, _dictionnary);
+	_createFunction = new Create(_javaType, _dictionnary);
 
-   _freeFunction = new Free(_javaType, _dictionnary);
+	_freeFunction = new Free(_javaType, _dictionnary);
 
-   nsC::Param::vector fields = _cStruct.getFields();
+	nsC::Param::vector fields = _cStruct.getFields();
 	string fieldsTemp;
-	string getterSetter;
 
-      _structSize = new Getter("struct_size",_javaType, _dictionnary, true);
-      _structSize->convertJava(f);
-      //g->convertJNI(f);
+	_structSize = new Getter("struct_size",_javaType, _dictionnary, true);
+	_structSize->convertJava(f);
 
 	for(size_t i =0; i<fields.size(); i++) {
-      string field("\t\tpublic %VALUE1% %VALUE2%;\n");
+		string field("\t\tpublic %VALUE1% %VALUE2%;\n");
 		if(fields[i].getCType() == "const void *" || fields[i].getCType() == "void *") {
-		   stringReplace(field, "VALUE1", _dictionnary->convertJava(fields[i].getCType() + "Array"));
-      }
-      else {
-		   stringReplace(field, "VALUE1", _dictionnary->convertJava(fields[i].getCType()));
-      }
+			stringReplace(field, "VALUE1", _dictionnary->convertJava(fields[i].getCType() + "Array"));
+		}
+		else {
+			stringReplace(field, "VALUE1", _dictionnary->convertJava(fields[i].getCType()));
+		}
 		stringReplace(field, "VALUE2", fields[i].getName());
 
-      Getter *g = new Getter(fields[i],_javaType, _dictionnary, false);
-      _getters.push_back(g);
-      Setter *s = new Setter(fields[i], _javaType,  _dictionnary);
-      _setters.push_back(s);
-      fieldsTemp += field;
-   }
+		Getter *g = new Getter(fields[i],_javaType, _dictionnary, false);
+		_getters.push_back(g);
+		Setter *s = new Setter(fields[i], _javaType,  _dictionnary);
+		_setters.push_back(s);
+		fieldsTemp += field;
+	}
 
-   stringReplace(structure, "CLASSNAME", _cStruct.getTypedef());
+	stringReplace(structure, "CLASSNAME", _cStruct.getTypedef());
 	stringReplace(structure, "FIELDS", fieldsTemp);
 	stringReplace(structure, "CONSTRUCTOR", generateConstructor());
-   stringReplace(structure, "FINALIZE", generateFinalize());
-   stringReplace(structure, "WRITE", generateWrite());
-   stringReplace(structure, "READ", generateRead());
+	stringReplace(structure, "FINALIZE", generateFinalize());
+	stringReplace(structure, "WRITE", generateWrite());
+	stringReplace(structure, "READ", generateRead());
 
-   string mInternal (
-            "\t\tAddressWrapper getMInternal() {\n"
-            "\t\t\treturn mInternal;\n"
-            "\t\t}\n\n"
-            );
-   stringReplace(structure, "MINTERNAL", mInternal);
-   f << structure;
+	string mInternal (
+			"\t\tAddressWrapper getMInternal() {\n"
+			"\t\t\treturn mInternal;\n"
+			"\t\t}\n\n"
+			);
+	stringReplace(structure, "MINTERNAL", mInternal);
+	f << structure;
 
-   _createFunction->convertJava(f);
+	//Write Function create/free/getters/setters for the struct.
+	_createFunction->convertJava(f);
 
-   _freeFunction->convertJava(f);
+	_freeFunction->convertJava(f);
 
-   for(size_t i = 0;i<_getters.size();i++)
-   {
-   		_getters[i]->convertJava(f);
-   		f << endl;
-   }
+	for(size_t i = 0;i<_getters.size();i++)
+	{
+		_getters[i]->convertJava(f);
+		f << endl;
+	}
 
-   for(size_t i = 0;i<_setters.size();i++)
-   {
-   		_setters[i]->convertJava(f);
-   		f << endl;
-   }
+	for(size_t i = 0;i<_setters.size();i++)
+	{
+		_setters[i]->convertJava(f);
+		f << endl;
+	}
 }
 
 string Struct::generateConstructor()
 {
 	string constructor(
-            "\t\tpublic %CLASSNAME%() {\n"
-            "\t\t\tmInternal = new AddressWrapper();\n"
-	         "\t\t\tmInternal.setAddress(gen_jni_%CLASSNAME%_create());\n"
-            "\t\t}\n\n"
-            );
-	
+			"\t\tpublic %CLASSNAME%() {\n"
+			"\t\t\tmInternal = new AddressWrapper();\n"
+			"\t\t\tmInternal.setAddress(gen_jni_%CLASSNAME%_create());\n"
+			"\t\t}\n\n"
+			);
+
 	stringReplace(constructor, "CLASSNAME", _cStruct.getTypedef());
 
 	return constructor;
@@ -136,13 +134,13 @@ string Struct::generateConstructor()
 string Struct::generateFinalize()
 {
 	string constructor(
-            "\t\t@Override\n"
-            "\t\tpublic void finalize() {\n"
-	         "\t\t\tgen_jni_%CLASSNAME%_free(mInternal.getAddress());\n"
-            "\t\t\tmInternal = null;\n"
-            "\t\t}\n\n"
-            );
-	
+			"\t\t@Override\n"
+			"\t\tpublic void finalize() {\n"
+			"\t\t\tgen_jni_%CLASSNAME%_free(mInternal.getAddress());\n"
+			"\t\t\tmInternal = null;\n"
+			"\t\t}\n\n"
+			);
+
 	stringReplace(constructor, "CLASSNAME", _cStruct.getTypedef());
 
 	return constructor;
@@ -151,22 +149,21 @@ string Struct::generateFinalize()
 string Struct::generateWrite()
 {
 	string write(
-            "\t\tpublic void write() {\n"
-	         "%FIELDS%"
-            "\t\t}\n\n"
-            );
+			"\t\tpublic void write() {\n"
+			"%FIELDS%"
+			"\t\t}\n\n"
+			);
 
-
-   nsC::Param::vector fields = _cStruct.getFields();
+	nsC::Param::vector fields = _cStruct.getFields();
 	string fieldsTemp;
 	for(size_t i =0; i<fields.size(); i++) {
-      string field("\t\t\tgen_jni_%CLASSNAME%_set_%VALUE%(mInternal.getAddress(), %VALUE%);\n");
-		
+		string field("\t\t\tgen_jni_%CLASSNAME%_set_%VALUE%(mInternal.getAddress(), %VALUE%);\n");
+
 		stringReplace(field, "VALUE", fields[i].getName());
-	   stringReplace(field, "CLASSNAME", _cStruct.getTypedef());
+		stringReplace(field, "CLASSNAME", _cStruct.getTypedef());
 		fieldsTemp += field;
-   }
-	
+	}
+
 	stringReplace(write, "FIELDS", fieldsTemp);
 
 	return write;
@@ -175,20 +172,20 @@ string Struct::generateWrite()
 string Struct::generateRead()
 {
 	string read(
-            "\t\tpublic void read() {\n"
-	         "%FIELDS%"
-            "\t\t}\n\n"
-            );
+			"\t\tpublic void read() {\n"
+			"%FIELDS%"
+			"\t\t}\n\n"
+			);
 
 
-   nsC::Param::vector fields = _cStruct.getFields();
+	nsC::Param::vector fields = _cStruct.getFields();
 	string fieldsTemp;
 	for(size_t i =0; i<_getters.size(); i++) {
 		fieldsTemp += "\t\t\t " + fields[i].getName() + " = " + _getters[i]->call() + "\n";
-   }
-   fieldsTemp += "\t\t\tstruct_size = gen_jni_%CLASSNAME%_get_struct_size();\n";
-	
-   stringReplace(fieldsTemp, "CLASSNAME", _cStruct.getTypedef());
+	}
+	fieldsTemp += "\t\t\tstruct_size = gen_jni_%CLASSNAME%_get_struct_size();\n";
+
+	stringReplace(fieldsTemp, "CLASSNAME", _cStruct.getTypedef());
 	stringReplace(read, "FIELDS", fieldsTemp);
 
 	return read;
@@ -215,7 +212,6 @@ bool Struct::isBooleanWrapper()
 
 void Struct::prepareCall(ofstream &f,string& varName)
 {
-
 }
 
 bool Struct::isArray()
@@ -225,7 +221,7 @@ bool Struct::isArray()
 
 string Struct::getJNIParameterName(string& varName)
 {
-   return "C_" + varName;
+	return "C_" + varName;
 }
 
 void Struct::getReturnValue(ofstream& f)
@@ -234,20 +230,17 @@ void Struct::getReturnValue(ofstream& f)
 
 vector<Function*> Struct::getGetterSetters()
 {
-   _getterSetters.push_back(_createFunction);
-   _getterSetters.push_back(_freeFunction);
-   _getterSetters.push_back(_structSize);
+	_getterSetters.push_back(_createFunction);
+	_getterSetters.push_back(_freeFunction);
+	_getterSetters.push_back(_structSize);
 
 	int size = _getterSetters.size();
 	copy(_getters.begin(),_getters.end(),back_inserter(_getterSetters));
 	assert(_getterSetters.size()==size+_getters.size());
-	
+
 	size = _getterSetters.size();
 	copy(_setters.begin(),_setters.end(),back_inserter(_getterSetters));
 	assert(_getterSetters.size()==size+_setters.size());
-	cout << "Size Getter = " << _getters.size() << endl;
-	cout << "Size Setter = " << _setters.size() << endl;
-	cout << "Size GetterSetter = " << _getterSetters.size() << endl; 
-	
+
 	return _getterSetters;
 }        
--- JNA/JNI/Types/Struct.h
+++ JNA/JNI/Types/Struct.h
@@ -4,10 +4,10 @@
  */
 
 /*!
-	\file Struct.h
-	\author Baptiste HAUDEGAND & Antoine MOISE
-	\version 1.0
-!*/
+  \file Struct.h
+  \author Baptiste HAUDEGAND & Antoine MOISE
+  \version 1.0
+  !*/
 #ifndef __JNI_STRUCT_H
 #define __JNI_STRUCT_H
 #include <string>
@@ -21,64 +21,83 @@
  * @{ */
 
 /*!
-	\namespace nsJNI 
-	JNI parser namespace
-!*/
+  \namespace nsJNI 
+  JNI parser namespace
+  !*/
 namespace nsJNI {
 
-	 class TypesDictionnary;
-    class Getter;
-    class Setter;
-    class Create;
-    class Free;
-    class Function;
-	 /*!
-		\class Struct
-		\brief represents a Struct
-	*/
-    class Struct : public Type 
-    {
-      public:
-      /*!
-      	\brief destructor
-      */
-        virtual ~Struct();
-		Struct(ofstream &f, ofstream& f2, const string& vmSignature,const nsC::Struct& cStruct,TypesDictionnary *dictionnary);
-		std::string outputJava();
-     	void addStructToJava(ofstream &f);
-     	void addStructFunctionToJNI(ofstream &f);
-        std::string outputJNI();
-        bool isNativeType();
-        bool isAddressWrapper();
-        bool isBooleanWrapper();
-        bool isArray();
-        void prepareCall(ofstream& f,string& varName);
-        string getJNIParameterName(string& varName);
-		/*!
-			\brief Generate 
-			\return the JNI Type
-		*/
-        string generateGetter(bool java, const string& fieldType,const string& fieldName);
-        string generateSetter(bool java, const string& fieldType,const string& fieldName);
-        string generateConstructor();
-        string generateFinalize();
-        string generateFreeFunction(bool java);
-        string generateWrite();
-        string generateRead();
-        string generateCreateFunction(bool java);
-        void getReturnValue(ofstream& f);
-        vector<Function*> getGetterSetters();
-      protected:
-        nsC::Struct _cStruct;
-        TypesDictionnary* _dictionnary;
-      	vector<Getter*> _getters;
-      	vector<Setter*> _setters;
-      	vector<Function*> _getterSetters;
-         Create *_createFunction;
-         Getter *_structSize;
-         Free *_freeFunction;
-    };
+	class TypesDictionnary;
+	class Getter;
+	class Setter;
+	class Create;
+	class Free;
+	class Function;
+	/*!
+	  \class Struct
+	  \brief represents a Struct
+	  */
+	class Struct : public Type 
+	{
+		public:
+			/*!
+			  \brief destructor
+			  */
+			virtual ~Struct();
+			Struct(ofstream &f, ofstream& f2, const string& vmSignature,const nsC::Struct& cStruct,TypesDictionnary *dictionnary);
+			std::string outputJava();
+			/*!
+			  \brief Write the structure to a Java file.
+			  */
+			void addStructToJava(ofstream &f);
+			/*!
+			  \brief Write the structure to the JNI file.
+			  */
+			void addStructFunctionToJNI(ofstream &f);
+			std::string outputJNI();
+			bool isNativeType();
+			bool isAddressWrapper();
+			bool isBooleanWrapper();
+			bool isArray();
+			void prepareCall(ofstream& f,string& varName);
+			string getJNIParameterName(string& varName);
+			/*!
+			  \brief Generate constructor of the structure
+			  \return the constructor function to write
+			  */
+			string generateConstructor();
+			/*!
+			  \brief Generate finalize function of the structure to free it.
+			  \return the finalize function to write
+			  */
+			string generateFinalize();
+			/*!
+			  \brief Generate write function of the structure.
+			  Write all params from Java to the C structure.
+			  \return the write function to write
+			  */
+			string generateWrite();
+			/*!
+			  \brief Generate read function of the structure.
+			  Read all params from the C structure to Java.
+			  \return the read function to write
+			  */
+			string generateRead();
+			void getReturnValue(ofstream& f);
+			/*!
+			  \brief Get all the functions used by the structure.
+			  All the functions means, create/free/getters/setters.
+			  \return all the functions used by the structure.
+			  */
+			vector<Function*> getGetterSetters();
+		protected:
+			nsC::Struct _cStruct;
+			TypesDictionnary* _dictionnary; /*!< dictionnary of all type*/
+			vector<Getter*> _getters; /*!< all the getter functions*/
+			vector<Setter*> _setters; /*!< all the setter functions*/
+			vector<Function*> _getterSetters; /*!< all functions used in the structure*/
+			Create *_createFunction; /*!< create function of the structure*/
+			Getter *_structSize; /*!< getter function of the structure size*/
+			Free *_freeFunction; /*!< free function of the structure*/
+	};
 }
-
 #endif
-
