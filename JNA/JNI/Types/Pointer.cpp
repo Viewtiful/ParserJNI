90dff5f6319cec7ac6b89a244d3a0025f6142d35
Baptiste Haudegand <baptiste.haudegand@gmail.com>
2013-07-22 16:47:36 +0200

    Indentation

--- JNA/JNI/Types/Pointer.cpp
+++ JNA/JNI/Types/Pointer.cpp
@@ -14,13 +14,10 @@ Pointer::Pointer(const string& VMSignature,const string& CBaseType,TypesDictionn
 	this->_CBaseType = CBaseType;
 	this->_dictionnary = dictionnary;
 	_isNativeType = isNativeType;
-	
 }
-
 
 Pointer::~Pointer()
 {
-
 }
 
 std::string Pointer::outputJava()
@@ -57,46 +54,46 @@ bool Pointer::isAddressWrapper()
 
 void Pointer::prepareCall(ofstream& f,string& varName)
 {
-   //We have a pointer from not a native type, we get the address and cast it
-   //to the corresponding type.
-   if(!_isNativeType && _CBaseType != "size_t") {
-      string structure (
-            "\t\t%TYPE% %NAME%;\n"
-            "\t\tif(%CName% != 0) {\n"
-            "\t\t\t%NAME% = (%TYPE%)((contextWrapper *)%CName%)->ctxRef;\n"
-            "\t\t\t((contextWrapper *)%CName%)->env = env;\n"
-            "\t\t}\n"
-            "\t\telse\n"
-            "\t\t\t%NAME% = NULL;\n\n"
-            );
+	//We have a pointer from not a native type, we get the address and cast it
+	//to the corresponding type.
+	if(!_isNativeType && _CBaseType != "size_t") {
+		string structure (
+				"\t\t%TYPE% %NAME%;\n"
+				"\t\tif(%CName% != 0) {\n"
+				"\t\t\t%NAME% = (%TYPE%)((contextWrapper *)%CName%)->ctxRef;\n"
+				"\t\t\t((contextWrapper *)%CName%)->env = env;\n"
+				"\t\t}\n"
+				"\t\telse\n"
+				"\t\t\t%NAME% = NULL;\n\n"
+				);
 
-      string name = "C_" + varName;
+		string name = "C_" + varName;
 
-      stringReplace(structure, "TYPE", _CBaseType);
-      stringReplace(structure, "NAME", name);
-      stringReplace(structure, "CName", varName);
+		stringReplace(structure, "TYPE", _CBaseType);
+		stringReplace(structure, "NAME", name);
+		stringReplace(structure, "CName", varName);
 
-      f << structure;
-   }
-   else if(_CBaseType == "size_t") {
-      string structure (
-            "\t\t%TYPE% %NAME%;\n\n"
-            );
+		f << structure;
+	}
+	else if(_CBaseType == "size_t") {
+		string structure (
+				"\t\t%TYPE% %NAME%;\n\n"
+				);
 
-      string name = "C_" + varName;
-      stringReplace(structure, "TYPE", _CBaseType);
-      stringReplace(structure, "NAME", name);
+		string name = "C_" + varName;
+		stringReplace(structure, "TYPE", _CBaseType);
+		stringReplace(structure, "NAME", name);
 
-      f << structure;
-   }
+		f << structure;
+	}
 }
 
 string Pointer::getJNIParameterName(string& varName)
 {
-   return "C_" + varName;
+	return "C_" + varName;
 }
 
 void Pointer::getReturnValue(ofstream& f)
 {
 }
-        
+
