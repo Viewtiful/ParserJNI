9e706de5a56ab57fa8f32a6d07ac5fd7b4f18d71
Baptiste Haudegand <baptiste.haudegand@gmail.com>
2013-07-22 14:34:40 +0200

    Types dictionnary cleaned

--- JNA/JNI/TypesDictionnary.cpp
+++ JNA/JNI/TypesDictionnary.cpp
@@ -2,7 +2,7 @@
  *
  * Body for TypesDictionnary class
  */
- 
+
 #include "JNI/TypesDictionnary.h"
 #include <fstream>
 #include <cstdlib>
@@ -15,33 +15,29 @@ TYPESDICTIONNARY::TypesDictionnary(string filename)
 {
 	_filename = filename;
 	addBaseType(filename);
-	   
 }
 
 TYPESDICTIONNARY::~TypesDictionnary()
 {
-    _fcts.clear();
+	_fcts.clear();
 
-    for ( std::map<std::string, Type*>::iterator it = _conversionMap.begin(); it != _conversionMap.end(); ++it ) 
-        delete it->second;
-    _conversionMap.clear();
-
-
+	for ( std::map<std::string, Type*>::iterator it = _conversionMap.begin(); it != _conversionMap.end(); ++it ) 
+		delete it->second;
+	_conversionMap.clear();
 }
 
 void TYPESDICTIONNARY::addBaseType(string filename)
 {
-
-        //Const void* and void* are considered as Array
-        addToMap("const void *Array", new Array("const void *","[B",this));
+	//Const void* and void* are considered as Array
+	addToMap("const void *Array", new Array("const void *","[B",this));
 	addToMap("void *Array", new Array("void *","[B",this));
-        
-        //Particular base Type
-        //Const void* and void* are translated into jbyteArray
-        addToMap("const char *", new NativeType("String","jstring","Ljava/lang/String;",true));
+
+	//Particular base Type
+	//Const void* and void* are translated into jbyte
+	addToMap("const char *", new NativeType("String","jstring","Ljava/lang/String;",true));
 	addToMap("const void *", new NativeType("byte","jbyte","B",true));
 	addToMap("void *", new NativeType("byte","jbyte","B",true));
-	
+
 
 	//Adding base type
 	addToMap("short", new NativeType("short","jshort","S",true));
@@ -55,7 +51,7 @@ void TYPESDICTIONNARY::addBaseType(string filename)
 	addToMap("size_t", new NativeType("long","jlong","size_t","J",true));
 	addToMap("char *", new NativeType("byte","jbyte","B",true));
 	addToMap("bool *",new BoolWrapper("bool *", "L" + filename + "$BoolWrapper;"));	
-	
+
 	//Adding base Arrays
 	addToMap("shortArray", new Array("short","[S",this));
 	addToMap("intArray", new Array("int","[I",this));
@@ -65,17 +61,16 @@ void TYPESDICTIONNARY::addBaseType(string filename)
 	addToMap("boolArray", new Array("boolean","[Z",this));
 	addToMap("charArray", new Array("char","[B",this));
 	addToMap("size_tArray", new Array("long","[J",this));
-	
 }
 
 bool TYPESDICTIONNARY::isNativeType(const string &type)
 {
 	assert(_conversionMap.count(type)==1);
-        Type *object = getType(type);
+	Type *object = getType(type);
 	return object->isNativeType();
 }
 
-	
+
 
 Type* TYPESDICTIONNARY::getType(const string& Ctype) {
 	Type *object = NULL;
@@ -88,10 +83,8 @@ Type* TYPESDICTIONNARY::getType(const string& Ctype) {
 
 string TYPESDICTIONNARY::convertJava(const string& Ctype)
 {
-	cout << "Converting to Java : "<< Ctype << endl;
-        Type * object = getType(Ctype);
+	Type * object = getType(Ctype);
 	return object->outputJava();
-	
 }
 
 int TYPESDICTIONNARY::countAt(const string& Ctype)
@@ -101,10 +94,8 @@ int TYPESDICTIONNARY::countAt(const string& Ctype)
 
 string TYPESDICTIONNARY::convertJNI(const string& Ctype)
 {
-	cout << "Converting to JNI : "<< Ctype << endl;
 	Type *object = getType(Ctype);	
 	return object->outputJNI();
-	
 }
 
 string TYPESDICTIONNARY::convertVM(const string& Ctype)
@@ -119,40 +110,40 @@ void TYPESDICTIONNARY::addStruct(ofstream &f, ofstream &f2, const nsC::Struct::v
 	//Add a struct to the map
 	std::vector<Struct*> createdStruct;
 	for (nsC::Struct::vector::const_iterator iterator(structs.begin());
-         iterator != structs.end();
-         ++iterator)
-   	{
-     		const nsC::Struct CStruct(*iterator);
-     	
-      		bool haveTypedef(!CStruct.getTypedef().empty());
-      		bool haveFields(CStruct.getFields().size() > 0);
-      		bool isTypedefPointer(CStruct.getTypedefIndirection() == 1);
-      		bool isDeepPointer(CStruct.getTypedefIndirection() > 1);
+			iterator != structs.end();
+			++iterator)
+	{
+		const nsC::Struct CStruct(*iterator);
+
+		bool haveTypedef(!CStruct.getTypedef().empty());
+		bool haveFields(CStruct.getFields().size() > 0);
+		bool isTypedefPointer(CStruct.getTypedefIndirection() == 1);
+		bool isDeepPointer(CStruct.getTypedefIndirection() > 1);
 		assert(haveTypedef);
 
-		// In this case, we had a Pointer
-		if(haveTypedef && !haveFields && (isTypedefPointer || isDeepPointer))
-      			addToMap(CStruct.getTypedef(), new Pointer("J",CStruct.getTypedef(),this,false));
-      		else if(haveTypedef && haveFields && !isTypedefPointer && !isDeepPointer)
-      		{
+		// In this case, we have a Pointer
+		if(!haveFields && (isTypedefPointer || isDeepPointer))
+			addToMap(CStruct.getTypedef(), new Pointer("J",CStruct.getTypedef(),this,false));
+		else if( haveFields && !isTypedefPointer && !isDeepPointer)
+		{
 			//We add a real struct	
-      			Struct *s = new Struct(f, f2, "L"+_filename+"$"+CStruct.getTypedef() + ";",CStruct,this);
+			Struct *s = new Struct(f, f2, "L"+_filename+"$"+CStruct.getTypedef() + ";",CStruct,this);
 			addToMap(CStruct.getTypedef(), s);
 			createdStruct.push_back(s);
-        	}  	
-   	}
+		}  	
+	}
+
 	//Generate getter, setters for structs
-	for(size_t i = 0;i<createdStruct.size();i++)
+	for(size_t i = 0;i<createdStruct.size();++i)
 	{
 		Struct *current = createdStruct[i];
 		current->addStructToJava(f);
 		current->addStructFunctionToJNI(f2);
 		vector<nsJNI::Function*> getSet = current->getGetterSetters();
-      		copy(getSet.begin(),getSet.end(),back_inserter(_fcts));
-      		getSet.clear();
-      	}
+		copy(getSet.begin(),getSet.end(),back_inserter(_fcts));
+		getSet.clear();
+	}
 	createdStruct.clear();	
-  		
 }
 
 void TYPESDICTIONNARY::addEnums(ofstream &f, const nsC::Enum::vector &enums) {
