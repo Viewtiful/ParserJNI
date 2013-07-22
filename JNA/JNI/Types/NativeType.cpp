74911fdf240428e460305508f148f1f9777e9c51
Baptiste Haudegand <baptiste.haudegand@gmail.com>
2013-07-22 16:46:22 +0200

    Identation

--- JNA/JNI/Types/NativeType.cpp
+++ JNA/JNI/Types/NativeType.cpp
@@ -14,8 +14,8 @@ NativeType::NativeType(const string& javaType, const string& jniType,
 			jniType, VMSignature)
 {
 	_isNativeType = isNativeType;
-   _jniType = jniType;
-   _CType = javaType;
+	_jniType = jniType;
+	_CType = javaType;
 }
 
 NativeType::NativeType(const string& javaType, const string& jniType,const string& CType,
@@ -23,7 +23,7 @@ NativeType::NativeType(const string& javaType, const string& jniType,const strin
 			jniType, VMSignature)
 {
 	_isNativeType = isNativeType;
-   _jniType = jniType;
+	_jniType = jniType;
 	_CType = CType;
 }
 NativeType::~NativeType()
@@ -69,67 +69,67 @@ void NativeType::prepareCall(ofstream& f,string& varName)
 {
 	string type = getCType();
 	string name = "C_" + varName;
-      
-   //If it's not a string, we do the normal thing. Casting from JNI types to C.
-   if(_jniType != "jstring") {
-	   string structure (
-			   "\t\t%TYPE% %NAME% = (%TYPE%) %NAMEBASE%;\n\n"
-			   );
 
-      stringReplace(structure, "TYPE", type);
-      stringReplace(structure, "NAME", name);
-      stringReplace(structure, "NAMEBASE", varName);
+	//If it's not a string, we do the normal thing. Casting from JNI types to C.
+	if(_jniType != "jstring") {
+		string structure (
+				"\t\t%TYPE% %NAME% = (%TYPE%) %NAMEBASE%;\n\n"
+				);
 
-	   f << structure; 
-   }
-   //If it's a string, we get the string from Java.
-   else {
-      string structure (
-            "\t\t%TYPE% %NAME%;\n"
-            "\t\t%NAME% = (%TYPE%)(*env)->Get%TYPEMAJ%UTFChars(env, %CNAME%, NULL);\n"
-            "\t\tif(%NAME% == NULL) {\n"
-            "\t\t\t fprintf(stderr, \" Out of memory\");\n"
-            "\t\t\t exit(1);\n"
-            "\t\t}\n\n"
-            );
+		stringReplace(structure, "TYPE", type);
+		stringReplace(structure, "NAME", name);
+		stringReplace(structure, "NAMEBASE", varName);
 
-      string typeMaj = toJavaName(type, false, false, true);
+		f << structure; 
+	}
+	//If it's a string, we get the string from Java.
+	else {
+		string structure (
+				"\t\t%TYPE% %NAME%;\n"
+				"\t\t%NAME% = (%TYPE%)(*env)->Get%TYPEMAJ%UTFChars(env, %CNAME%, NULL);\n"
+				"\t\tif(%NAME% == NULL) {\n"
+				"\t\t\t fprintf(stderr, \" Out of memory\");\n"
+				"\t\t\t exit(1);\n"
+				"\t\t}\n\n"
+				);
 
-      stringReplace(structure, "TYPEMAJ", typeMaj);
-      stringReplace(structure, "NAME", name);
-      stringReplace(structure, "CNAME", varName);
-      stringReplace(structure, "TYPE", "char *");
+		string typeMaj = toJavaName(type, false, false, true);
 
-      f << structure;
-   }
+		stringReplace(structure, "TYPEMAJ", typeMaj);
+		stringReplace(structure, "NAME", name);
+		stringReplace(structure, "CNAME", varName);
+		stringReplace(structure, "TYPE", "char *");
+
+		f << structure;
+	}
 }
 
 string NativeType::getJNIParameterName(string& varName)
 {
-   return "C_" + varName;
+	return "C_" + varName;
 }
 
 void NativeType::getReturnValue(ofstream& f)
 {
-   //Return type == void to nothing.
+	//Return type == void to nothing.
 	if(_jniType == "void") 
 		return ;
-		
-   //Return type == string, create an object for Java and return it.
+
+	//Return type == string, create an object for Java and return it.
 	else if(_jniType == "jstring") 
 	{
-      string structure (
-               "\t\tJNI_result = (*env)->New%TYPEMAJ%UTF(env, tempJNI_result);\n"
-		         );
-      string type = getJNIType();
-      string typeMaj = type.substr(1, type.size());
-	 	typeMaj = toJavaName(typeMaj, false, false, true);
+		string structure (
+				"\t\tJNI_result = (*env)->New%TYPEMAJ%UTF(env, tempJNI_result);\n"
+				);
+		string type = getJNIType();
+		string typeMaj = type.substr(1, type.size());
+		typeMaj = toJavaName(typeMaj, false, false, true);
 
-      stringReplace(structure, "TYPEMAJ", typeMaj);
+		stringReplace(structure, "TYPEMAJ", typeMaj);
 
-      f << structure;
+		f << structure;
 	}
 
 	f << "\t\t return JNI_result;\n";
-	   
+
 }        
--- JNA/JNI/Types/Param.h
+++ JNA/JNI/Types/Param.h
@@ -2,12 +2,12 @@
  *
  * Header for JNIParser
  */
- 
+
 /*!
-	\file Param.h
-	\author Baptiste HAUDEGAND & Antoine MOISE
-	\version 1.0
-!*/
+  \file Param.h
+  \author Baptiste HAUDEGAND & Antoine MOISE
+  \version 1.0
+  !*/
 #ifndef __JNI_PARAM_H
 #define __JNI_PARAM_H
 #include <iostream>
@@ -15,35 +15,35 @@
 using namespace std;
 
 /*!
-	\namespace nsJNI 
-	JNI parser namespace
-!*/
+  \namespace nsJNI 
+  JNI parser namespace
+  !*/
 namespace nsJNI {
 
 	/*!
-		\class Param
-		\brief represents a Function Parameter
-	*/
-    class Param
-    {
-      public:
-      /*!
-        	\brief constructor with two parameter
-        	Create, and initialize a Param with the following arguments
-        	\param type : type of the new Parameter
-        	\param name : name of the new Parameter
-        */
-        Param(const string& type,const string& name);
-        /*!
-      	\brief destructor
-      */
-		virtual ~Param();
-		std::string& getType();
-        std::string& getName();
-      protected:
-      	string _type; /*!< Type of the parameter*/
-      	string _name; /*!< Name of the parameter*/
-    };
+	  \class Param
+	  \brief represents a Function Parameter
+	  */
+	class Param
+	{
+		public:
+			/*!
+			  \brief constructor with two parameter
+			  Create, and initialize a Param with the following arguments
+			  \param type : type of the new Parameter
+			  \param name : name of the new Parameter
+			  */
+			Param(const string& type,const string& name);
+			/*!
+			  \brief destructor
+			  */
+			virtual ~Param();
+			std::string& getType();
+			std::string& getName();
+		protected:
+			string _type; /*!< Type of the parameter*/
+			string _name; /*!< Name of the parameter*/
+	};
 }
 
 #endif
