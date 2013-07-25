7bccccf4e070658c66a600884d6d5657df11af0a
moise.antoine@hotmail.fr <moise.antoine@hotmail.fr>
2013-07-25 11:55:57 +0200

    Comments for Way.h

--- JNA/JNI/Types/Way.h
+++ JNA/JNI/Types/Way.h
@@ -1,18 +1,34 @@
+/*!
+ * \file Way.h
+ * \brief Prevent unused copy for JNI code generation
+ */
 #ifndef __JNI_WAY_H
 #define __JNI_WAY_H
 #include <string>
 #include <cctype>
 #include <iostream>
 using namespace std;
+
+/*!
+ * \enum Way
+ * \brief give informations of the use of an Parameter
+ */
 typedef enum
 {
-    IN,
-    OUT,
-    UNKNOWN
+    IN, /*!< If the parameter contains only input Data for function*/
+    OUT, /*!< If the parameter will contain output Data for function */
+    UNKNOWN /*!< We have no information*/
 }Way;
 
+/*!
+ * \brief compute the right Enum value with a string
+ * \param way : the enum walue in string format
+ */
 Way getWay(string way);
-
+/*!
+ * \brief transform an lowercase string to uppercase string
+ * \param s a lowercase string
+ */
 void toupper(string &s);
 
 
